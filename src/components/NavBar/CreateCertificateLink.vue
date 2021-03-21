6a8e8ffe04c0f0b8d6341e04c81ae394ef23c1c8
lukaDoric <luka.doric@uns.ac.rs>
2021-03-21 14:41:14 +0100

    Initial form for creating certificate.

---
+++ src/components/NavBar/CreateCertificateLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="nav-item active">
+    <router-link to="/createCertificate"><a class="nav-link" href="#">Create certificate<span
+        class="sr-only">(current)</span></a>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "CreateCertificateLink"
+}
+</script>
+
+<style scoped>
+
+</style>
