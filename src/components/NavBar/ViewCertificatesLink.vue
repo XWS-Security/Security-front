72a47fe1428ca75856b106a823bc34bb7ff6fbe6
Marko Vidovic <marko.vidovich@gmail.com>
2021-03-21 21:21:58 +0100

    Created table for revocation of certificates

---
+++ src/components/NavBar/ViewCertificatesLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="nav-item active">
+    <router-link to="/viewCertificates"><a class="nav-link" href="#">View Certificates<span
+        class="sr-only">(current)</span></a>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ViewCertificatesLink"
+}
+</script>
+
+<style scoped>
+
+</style>
