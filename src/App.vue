2f6b681f4bc4f37a78b374c28efe1e6c2aa6c126
Milica Siriski <milica.siriski998@gmail.com>
2021-04-18 20:30:20 +0200

    Registration form with basic frontend password validation

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <NavGroup side='mr-auto'>
         <CreateCertificateLink></CreateCertificateLink>
         <ViewCertificatesLink></ViewCertificatesLink>
+        <RegistrationLink></RegistrationLink>
       </NavGroup>
     </NavBar>
     <router-view></router-view>
@@ -15,10 +16,12 @@ import NavBar from './components/NavBar/NavBar.vue'
 import CreateCertificateLink from "@/components/NavBar/CreateCertificateLink";
 import ViewCertificatesLink from "./components/NavBar/ViewCertificatesLink";
 import NavGroup from "@/components/NavBar/NavGroup";
+import RegistrationLink from "@/components/NavBar/RegistrationLink";
 
 export default {
   name: 'App',
   components: {
+    RegistrationLink,
     CreateCertificateLink,
     ViewCertificatesLink,
     NavBar,
