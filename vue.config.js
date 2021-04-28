8b3be4bcf1ed8bc5bc16dc999c933928cbf73b64
Milica Siriski <milica.siriski998@gmail.com>
2021-04-28 21:46:10 +0200

    Password change for user that is signed in.

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 module.exports = {
     devServer: {
         https: {
-            key: fs.readFileSync('./certs/example.com+5-key.pem'),
-            cert: fs.readFileSync('./certs/example.com+5.pem'),
+            key: fs.readFileSync('./certificates/example.com+5-key.pem'),
+            cert: fs.readFileSync('./certificates/example.com+5.pem'),
         },
         public: 'https://localhost:8081/'
     }
