070c72e96a385b9bd7d5cb1be79d3b327c54cf45
Vladimir Budjen <bvalda88@gmail.com>
2021-06-04 16:57:33 +0200

    Made search by tags and location

--- src/components/Nistagram/Profile/ProfilePicture.vue
+++ src/components/Nistagram/Profile/ProfilePicture.vue
@@ -14,8 +14,8 @@ export default {
       image: null
     }
   },
-  watch:{
-    imageName: function(newVal, oldVal) {
+  watch: {
+    imageName: function (newVal, oldVal) {
       console.log('Prop changed: ', newVal, ' | was: ', oldVal)
       this.getBase64(newVal);
     }
@@ -24,26 +24,26 @@ export default {
     getBase64: function () {
 
       this.$http
-          .get(process.env.VUE_APP_CONTENT_URL  + 'image/' + this.imageName, {
+          .get(process.env.VUE_APP_CONTENT_URL + 'image/' + this.imageName, {
             responseType: 'arraybuffer'
           })
           .then(response => {
-            let type  = Object.values(response.headers)[1];
+            let type = Object.values(response.headers)[2];
             this.image = _arrayBufferToBase64(response.data, type)
           })
     },
-    redirectToStories: function (){
+    redirectToStories: function () {
 
     },
   }
 }
 </script>
 <style scoped>
-  img {
-    border-color: #e2e8f0;
-    border-radius: 50%;
-    width:100px;
-    height:100px;
-    margin: 3%;
-  }
+img {
+  border-color: #e2e8f0;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  margin: 3%;
+}
 </style>
