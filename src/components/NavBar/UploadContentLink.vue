d4342973ad8d28487f414ed8daab7b8b8a187914
lukaDoric <luka.doric@uns.ac.rs>
2021-06-06 21:00:12 +0200

    Added link for content upload.

---
+++ src/components/NavBar/UploadContentLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="nav-item active">
+    <router-link to="/contentUpload"><a class="nav-link" href="#">Upload content<span
+        class="sr-only">(current)</span></a>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+name: "UploadContentLink"
+}
+</script>
+
+<style scoped>
+
+</style>
