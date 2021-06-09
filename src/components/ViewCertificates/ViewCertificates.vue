64a4447f26346f648898d7b65afe64de01e4e8d1
lukaDoric <luka.doric@uns.ac.rs>
2021-06-09 19:34:03 +0200

    User is able to download certificate.

--- src/components/ViewCertificates/ViewCertificates.vue
+++ src/components/ViewCertificates/ViewCertificates.vue
@@ -27,9 +27,21 @@
                   class="btn btn-danger">
             Revoke
           </button>
+          <button @click="downloadCertificate(certificate.certificateName)" :value="certificate.certificateName"
+                  type="button"
+                  class="btn btn-success">
+            Download
+          </button>
         </td>
         </tbody>
       </table>
+    </b-jumbotron>
+    <b-jumbotron>
+      <div>
+        <label for="CertificateAlias">Keystore password</label>
+        <input type="text" v-model="keystorePass" class="form-control" id="CertificateAlias"
+               aria-describedby="emailHelp">
+      </div>
     </b-jumbotron>
   </div>
 </template>
@@ -41,6 +53,7 @@ export default {
     return {
       selected: '',
       certificates: [],
+      keystorePass: ''
     }
   },
 
@@ -74,6 +87,18 @@ export default {
         'certificateName': certificateName,
       }).then(response => {
         console.log(response.data);
+        this.init();
+      }).catch(err => {
+        alert(err.response.data)
+      });
+    },
+
+    downloadCertificate(certificateName) {
+
+      let downloadCertificate = { certificateName: certificateName, keystorePass: this.keystorePass }
+
+      this.$http.post(process.env.VUE_APP_BACKEND_URL + 'certificate/download/', downloadCertificate).then(response => {
+        alert(response.data)
         this.init();
       }).catch(err => {
         alert(err.response.data)
