99168dc28ce6fdb9e78b914877b99427ea420e32
lukaDoric <luka.doric@uns.ac.rs>
2021-06-11 16:19:10 +0200

    Certificates deploy

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 module.exports = {
     devServer: {
         https: {
-            key: fs.readFileSync('./certificates/end-key.pem'),
-            cert: fs.readFileSync('./certificates/end-cert.pem'),
+            key: fs.readFileSync(process.env.VUE_APP_KEY),
+            cert: fs.readFileSync(process.env.VUE_APP_CERT),
         },
         public: 'https://localhost:8081/'
     }
