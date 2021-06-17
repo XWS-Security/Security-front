7f4ed6f207ba51c3818715c08ff79754c610b964
lukaDoric <luka.doric@uns.ac.rs>
2021-06-17 17:17:36 +0200

    Admin is able to see reported posts.

---
+++ src/components/NavBar/ReportedLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="nav-item active">
+    <router-link to="/reportedPosts"><a class="nav-link" href="#">Reported Posts<span
+        class="sr-only">(current)</span></a>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ReportedLink",
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Nistagram/Post/ReportedPosts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex align-content-around  justify-content-center flex-wrap light_blue">
+    <div></div>
+    <post-image v-for="p in posts" :key="p.id"
+                v-bind:imageName="p.imageName"
+                v-bind:id="p.id"
+                v-bind:user="p.userId"
+                v-bind:profile-img="p.userProfileImageName"></post-image>
+  </div>
+</template>
+
+<script>
+import PostImage from "@/components/Nistagram/Profile/PostImage";
+
+export default {
+  components: {PostImage},
+  name: "Reported",
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    this.$http
+        .get(process.env.VUE_APP_CONTENT_URL + 'report/post')
+        .then(response => {
+          this.posts = response.data;
+        })
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+
+</style>
