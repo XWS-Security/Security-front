c7f1947a2a5035aab519ae682f75adf57e0a3c98
Milica Siriski <milica.siriski998@gmail.com>
2021-06-17 17:37:54 +0200

    Content from subscribed users is loaded on home page

--- src/components/User/InstagramUserHome.vue
+++ src/components/User/InstagramUserHome.vue
@@ -1,14 +1,39 @@
 <template>
-  <img alt="" src="../../assets/instagram.png" style="margin: 1% 10% 10% 10%">
+  <div class="col">
+    <div class="row" v-for="(c, index) in content" v-bind:key="index">
+      <div class="col-1"></div>
+      <div class="col-8">
+        <subscribed-content class="my-3" v-bind:content-id="c.postId" v-bind:username="c.username"
+                            v-bind:profile-image="c.profileImage"></subscribed-content>
+      </div>
+      <div class="col-2"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+
+import SubscribedContent from "@/components/Nistagram/Home/SubscribedContent";
+
 export default {
   name: "InstagramUserHome",
+  components: {SubscribedContent},
+  data() {
+    return {
+      content: []
+    }
+  },
   mounted() {
-    if (this.$store.state.userType !== 'InstagramUser') {
+    if (this.$store.state.userType !== 'NistagramUser') {
       this.$router.push("/")
     }
+
+    this.$http
+        .get(process.env.VUE_APP_CONTENT_URL + 'home/')
+        .then(response => {
+          this.content = response.data
+        })
+        .catch(err => (console.log(err)));
   },
 }
 </script>
