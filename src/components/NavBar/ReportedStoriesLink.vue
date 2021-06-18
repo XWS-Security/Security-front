06dca9a3acc9ed1f202acc905115fafa58e62c5f
lukaDoric <luka.doric@uns.ac.rs>
2021-06-18 11:53:13 +0200

    User is able to report story.

---
+++ src/components/NavBar/ReportedStoriesLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="nav-item active">
+    <router-link to="/story?&storiesType=reported"><a class="nav-link" href="#">Reported Stories<span
+        class="sr-only">(current)</span></a>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ReportedStoriesLink",
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/Nistagram/Story/Story.vue
+++ src/components/Nistagram/Story/Story.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="background-color: #2e2e2e;color: #e2e8f0" >
+  <div style="background-color: #2e2e2e;color: #e2e8f0">
     <div class="container-lg">
       <div class="row-cols-6">
         <div class="col-lg-1 float-right">
@@ -8,7 +8,7 @@
         </div>
         <div class="col-lg-2" v-on:click="backToProfile">
           <br>
-          <img  style="background-color: #2e2e2e" class="profileImage" v-bind:src="profileImage"/>  {{username}}
+          <img style="background-color: #2e2e2e" class="profileImage" v-bind:src="profileImage"/> {{ username }}
         </div>
       </div>
       <hr style="background-color: #e2e8f0">
@@ -26,8 +26,18 @@
             <b-carousel-slide v-for="(s, index) in storiesWithImages" v-bind:key="index" img-blank
                               img-alt="Blank image">
 
-              {{formatDateWithHours(s.story.date)}} <span v-for="tag in s.story.tags" v-bind:key="tag"><a href="">{{tag}} </a>
-            </span> <span v-if="s.story.location!=null"> at <a href=""> {{s.story.location.name}} </a></span>
+              <b-jumbotron id="report" v-if="reportVisible">
+                <textarea class="form-control" type="text" placeholder="Report reason" v-model="reason"></textarea>
+                <div class="input-group-append">
+                  <button class="btn btn-danger" v-on:click="sendReport(s.story.id)">Send report</button>
+                </div>
+              </b-jumbotron>
+              {{ formatDateWithHours(s.story.date) }} <span v-for="tag in s.story.tags" v-bind:key="tag"><a
+                href="">{{ tag }} </a>
+            </span> <span v-if="s.story.location!=null"> at <a href=""> {{ s.story.location.name }} </a></span>
+              <button v-if="user === 'NistagramUser'" class="btn btn-danger mr-2" @click="setReportVisibility">Report</button>
+              <button v-if="user === 'Administrator'" class="btn btn-danger mr-2">Remove Post</button>
+              <button v-if="user === 'Administrator'" class="btn btn-danger mr-2">Remove User</button>
               <img v-if="s.mediatype==='image/jpeg'" class="item" v-bind:src="s.url"/>
               <video controls v-else class="item" v-bind:src="s.url" autoplay loop v-bind:muted="index!=selected"/>
             </b-carousel-slide>
@@ -35,46 +45,51 @@
         </div>
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script>
 import {_arrayBufferToBase64} from "@/assets/js/HellperFunctions";
 import {formatDate} from "@/assets/js/HellperFunctions";
+
 export default {
   name: "Story",
   data() {
     return {
-      medias:{},
-      stories:[],
-      storiesWithImages:[],
-      selected:0,
-      username:"",
-      storiesType:"",
-      profileImageName:null,
-      profileImage:null
+      medias: {},
+      stories: [],
+      storiesWithImages: [],
+      selected: 0,
+      username: "",
+      storiesType: "",
+      profileImageName: null,
+      profileImage: null,
+      reportVisible: false,
+      reason: ''
     }
   },
-  computed:{
-    linkForGettingStories: function(){
-      if(this.storiesType==='highlights'){
+  computed: {
+    linkForGettingStories: function () {
+      if (this.storiesType === 'highlights') {
         return process.env.VUE_APP_CONTENT_URL + 'story/highlights/' + this.username;
-      }
-      else if(this.storiesType==='my'){
+      } else if (this.storiesType === 'my') {
         return process.env.VUE_APP_CONTENT_URL + 'story/my';
-      }
-      else{
+      } else if (this.storiesType === 'reported') {
+        return process.env.VUE_APP_CONTENT_URL + 'report/story';
+      } else {
         return process.env.VUE_APP_CONTENT_URL + 'story/' + this.username;
       }
+    },
+    user() {
+      let user = this.$store.state.userType;
+      return user;
     }
   },
   mounted() {
     this.getProfileData();
     this.getStories();
   },
-  methods:{
+  methods: {
     getStories() {
       this.$http
           .get(this.linkForGettingStories)
@@ -87,13 +102,12 @@ export default {
       this.username = urlParams.get('username');
       this.profileImageName = urlParams.get('profileImage');
       this.storiesType = urlParams.get('storiesType');
-      if(this.storiesType==='my'){
+      if (this.storiesType === 'my') {
         this.getLoggedUserInfo();
-      }
-      else
+      } else
         this.getProfilePicture();
     },
-    getLoggedUserInfo(){
+    getLoggedUserInfo() {
       this.$http
           .get(process.env.VUE_APP_CONTENT_URL + 'interaction/loggedUser')
           .then(response => {
@@ -102,7 +116,7 @@ export default {
             this.getProfilePicture();
           })
     },
-    getProfilePicture(){
+    getProfilePicture() {
 
       this.$http
           .get(process.env.VUE_APP_CONTENT_URL + 'image/' + this.profileImageName, {
@@ -121,19 +135,32 @@ export default {
                 responseType: 'arraybuffer'
               })
               .then(response => {
-                let type  = response.headers['content-type'];
-                let story = {url: _arrayBufferToBase64(response.data, type), mediatype: type, story:s};
+                let type = response.headers['content-type'];
+                let story = {url: _arrayBufferToBase64(response.data, type), mediatype: type, story: s};
                 this.storiesWithImages.push(story)
               })
       )
     },
-    formatDateWithHours(date){
+    formatDateWithHours(date) {
       let d = new Date(date);
       return formatDate(date) + " at " + d.getHours() + ":" + d.getMinutes();
     },
-    backToProfile(){
+    backToProfile() {
       this.$router.push('/nistagramprofile?id=' + this.username)
-    }
+    },
+    sendReport(id) {
+      let reportData = {reason: this.reason, contentId: id, reportType: 'STORY'}
+
+      this.$http.post(process.env.VUE_APP_CONTENT_URL + 'report/', reportData)
+          .then(response => {
+            alert(response.data)
+          })
+          .catch(err => (this.console.log(err.data)))
+    },
+
+    setReportVisibility() {
+      this.reportVisible = !this.reportVisible;
+    },
   }
 }
 </script>
@@ -143,7 +170,8 @@ export default {
   width: 99%;
   height: 99%;
 }
-.profileImage{
+
+.profileImage {
   border-color: #e2e8f0;
   border-radius: 50%;
   width: 80px;
