1c764515a74238929afac4ba15feb729caea3edf
Vladimir Budjen <bvalda88@gmail.com>
2021-07-03 20:49:07 +0200

    Influencer can accept or reject hire requests

--- src/components/Campaign/Influencer.vue
+++ src/components/Campaign/Influencer.vue
@@ -5,7 +5,7 @@
     <br>
     <b-button v-if="status==='PUBLIC'||status==='FOLLOWING'" variant="warning" @click="hire">Hire</b-button>
     <b-button v-else-if="status==='NOT_FOLLOWING'" variant="warning" @click="follow">Follow</b-button>
-    <h4 v-else-if="status==='REQUEST_SENT'" >Waiting for request approve</h4>
+    <h4 v-else-if="status==='REQUEST_SENT'" >Waiting for friend request approve</h4>
   </b-jumbotron>
 </template>
 
@@ -29,19 +29,34 @@ name: "Influencer",
             this.status = response.data
           })
     },
-    getFollowingStatus(){
+    follow() {
+      let data = {username: this.username}
+      this.$http
+          .post(process.env.VUE_APP_FOLLOWER_URL + 'interactions/', data)
+          .then(response => {
 
-    },
-    follow(){
-
+            this.status= response.data;
+          })
+          .catch(err => {
+            err.response.data
+            alert("Something went wrong!")
+          })
     },
     hire(){
+      if(this.campaign===undefined || this.campaign===null || this.campaign===''){
+        alert("You must chose campaign!")
+        return
+      }
+      if(Object.keys(this.campaign).length===0){
+        alert("You must chose campaign!")
+        return
+      }
       let dto = {influencerUsername:this.username, campaignId:this.campaign.id};
       this.$http
           .post(process.env.VUE_APP_CAMPAIGN_URL + 'hireRequest/',dto)
           .then(response => {
-            alert(response.data)
-          })
+            alert(response.data);
+          }).catch(err=>{alert(err.response.data)})
     }
   },
   watch:{
