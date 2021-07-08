50fd33f2ca7ad28c648a37536fd4bb7a78d0d274
Vladimir Budjen <bvalda88@gmail.com>
2021-07-08 23:22:15 +0200

    Adds on agents profile page

--- src/components/Nistagram/Profile/PostImage.vue
+++ src/components/Nistagram/Profile/PostImage.vue
@@ -2,6 +2,7 @@
   <div class="item">
     <img v-if="media.mediatype==='image/jpeg'" class="item" v-bind:src="media.url" v-on:click="openPost"/>
     <video v-else class="item" id="vid" v-bind:src="media.url" autoplay loop muted v-on:click="openPost"/>
+    <b-button v-if="campaignId!==undefined" class="link" block variant="info" size="sm" v-on:click="openLink">Visit add</b-button>
   </div>
 </template>
 
@@ -11,25 +12,40 @@ import {_arrayBufferToBase64} from "@/assets/js/HellperFunctions";
 export default {
   name: "PostImage",
   mounted() {
-    this.getBase64(this.imageName);
+    this.img= this.imageName;
+    this.getImage(this.img);
+    if(this.oneTime){
+      this.seeAdvertisements();
+    }
   },
-  props: ['imageName', 'id', 'user', 'profileImg', 'clickable'],
+  props: ['imageName', 'id', 'user', 'profileImg', 'clickable', 'campaignId', 'oneTime'],
   data() {
     return {
-      media: {mediatype: '', url: ''}
+      media: {mediatype: '', url: ''},
+      img:undefined
     }
   },
   watch: {
     imageName: function (newVal, oldVal) {
       console.log('Prop changed: ', newVal, ' | was: ', oldVal)
+      this.img = newVal;
       this.getBase64(newVal);
     }
   },
   methods: {
+    seeAdvertisements(){
+      if(this.campaignId)
+      this.$http
+          .put(process.env.VUE_APP_CAMPAIGN_URL + 'advertisement/onetime/see/' + this.campaignId)
+          .then(response => {
+            console.log(response)
+          })
+          .catch(err => (console.log(err)));
+    },
     getBase64: function () {
 
       this.$http
-          .get(process.env.VUE_APP_CONTENT_URL + 'image/' + this.imageName, {
+          .get(process.env.VUE_APP_CONTENT_URL + 'image/' + this.img, {
             responseType: 'arraybuffer'
           })
           .then(response => {
@@ -37,6 +53,24 @@ export default {
             this.media.url = _arrayBufferToBase64(response.data, type)
             this.media.mediatype = type;
           })
+    },
+    getImageNameThenGetBase64(){
+      this.$http
+          .get(process.env.VUE_APP_CONTENT_URL + 'post/images/' + this.id)
+          .then(response => {
+            this.img=response.data[0];
+            this.getImage();
+          })
+    },
+    getImage:function (){
+      if(this.img===undefined){
+        this.getImageNameThenGetBase64();
+      }
+      else
+      this.getBase64();
+    },
+    openLink(){
+
     },
     openPost: function () {
       if (!this.clickable) return
@@ -46,14 +80,22 @@ export default {
             .then(response => {
               let user = response.data.username;
               let profileImg = response.data.profileImg;
-              this.$router.push("/post?id=" + this.id + '&username=' + user + '&profileImg=' + profileImg);
+              this.$router.push("/post?id=" + this.id + '&username=' + user + '&profileImg=' + profileImg + this.campaignUrlPart);
             })
       } else {
-        this.$router.push("/post?id=" + this.id + '&username=' + this.user + '&profileImg=' + this.profileImg);
+        this.$router.push("/post?id=" + this.id + '&username=' + this.user + '&profileImg=' + this.profileImg + this.campaignUrlPart);
       }
     },
     getUserInfo() {
-
+      //TODO: go to address and make click event
+    }
+  },
+  computed:{
+    campaignUrlPart(){
+      if(this.campaignId!==undefined){
+        return '&campaignId=' + this.campaignId;
+      }
+      return ''
     }
   }
 }
@@ -63,7 +105,7 @@ export default {
 .item {
   width: 180px;
   height: 180px;
-  margin: 1%;
+  margin: 3%;
 }
 
 </style>
