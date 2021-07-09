299f82534ad5b7cfe9cae40309c02d5b3757d5a1
lukaDoric <luka.doric@uns.ac.rs>
2021-07-09 11:57:40 +0200

    User is able to manage notificaitons.

---
+++ src/components/Nistagram/Notifications/NotificationSettings.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <hr>
+    <h4>Configure notifications for likes</h4>
+    <input type="radio" id="On" value="ON" v-model="likeNotifications">
+    <label for="On">On</label>
+    <br>
+    <input type="radio" id="Off" value="OFF" v-model="likeNotifications">
+    <label for="Off">Off</label>
+    <br>
+    <input type="radio" id="OnOnlyForFollowers" value="FOLLOWERS_ON" v-model="likeNotifications">
+    <label for="OnOnlyForFollowers">On only for followers</label>
+    <br>
+    <b-button class="btn btn-success" @click="updateLikeNotifications">UPDATE</b-button>
+    <hr>
+    <h4>Configure notifications for comments</h4>
+    <input type="radio" id="CommentsOn" value="ON" v-model="commentNotifications">
+    <label for="On">On</label>
+    <br>
+    <input type="radio" id="CommentsOff" value="OFF" v-model="commentNotifications">
+    <label for="Off">Off</label>
+    <br>
+    <input type="radio" id="CommentsOnOnlyForFollowers" value="FOLLOWERS_ON" v-model="commentNotifications">
+    <label for="OnOnlyForFollowers">On only for followers</label>
+    <br>
+    <b-button class="btn btn-success" @click="updateCommentNotifications">UPDATE</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotificationSettings",
+
+  data() {
+    return {
+      commentNotifications: '',
+      likeNotifications: ''
+    }
+  },
+
+  methods: {
+
+    updateLikeNotifications() {
+      let notificationDto = { 'notificationStatus': this.likeNotifications};
+
+      this.$http
+          .put(process.env.VUE_APP_CONTENT_URL + 'notification/like', notificationDto)
+    },
+
+    updateCommentNotifications() {
+      let notificationDto = { 'notificationStatus': this.commentNotifications};
+
+      this.$http
+          .put(process.env.VUE_APP_CONTENT_URL + 'notification/comment', notificationDto)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Nistagram/Notifications/Notifications.vue
+++ src/components/Nistagram/Notifications/Notifications.vue
@@ -2,6 +2,10 @@
   <div>
     <b-card no-body>
       <b-tabs card>
+        <b-tab title="Configure Notifications">
+          <b-card-text>Configure section</b-card-text>
+          <NotificationSettings></NotificationSettings>
+        </b-tab>
         <b-tab title="Followers" active>
           <FollowRequests></FollowRequests>
         </b-tab>
@@ -25,10 +29,11 @@
 import FollowRequests from "@/components/Nistagram/Notifications/FollowRequests";
 import LikedPosts from "@/components/Nistagram/Notifications/LikedPosts";
 import DislikedPosts from "@/components/Nistagram/Notifications/DislikedPosts";
+import NotificationSettings from "@/components/Nistagram/Notifications/NotificationSettings";
 
 export default {
   name: "Notifications",
-  components: {DislikedPosts, LikedPosts, FollowRequests}
+  components: {NotificationSettings, DislikedPosts, LikedPosts, FollowRequests}
 }
 </script>
 
